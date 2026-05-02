76a7d91c261205d5b1df3a74ebd390ae5a97d904
wwshoaf <wws00002@mix.wvu.edu>
2026-05-02 19:09:59 -0400

    index for number of classes taught for teacher

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,3 +1,20 @@
-/* Create non-primary indexes
-sql/indexes.sql · e.g. Class(Date), Sale(Date), PersonClass(Date, Time)
-*/
+/* Create non-primary indexes */
+-- Drop index to see before an after
+DROP INDEX idx_teacher_classes ON Teacher;
+
+-- Check performance by running EXPLAIN before index
+EXPLAIN SELECT name, phone, number_of_classes_taught
+FROM Teacher
+ORDER BY number_of_classes_taught DESC;
+
+-- Index on Teacher.number_of_classes_taught
+CREATE INDEX idx_teacher_classes
+ON Teacher(number_of_classes_taught);
+
+-- Check performance by running EXPLAIN after index
+EXPLAIN SELECT name, phone, number_of_classes_taught
+FROM Teacher
+ORDER BY number_of_classes_taught DESC;
+
+-- Show the index
+SHOW INDEX FROM Teacher;
